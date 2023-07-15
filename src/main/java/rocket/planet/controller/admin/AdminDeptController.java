8357641f707f3d8d6baec5397e943957ae76181e
jiokim-7 <now20412041@gmail.com>
2023-07-15 21:54:52 +0900

    [PN-103] feat: 부문 및 팀 생성 수정 삭제 기능
    - 컨트롤러 주석 해제 및 수정

--- src/main/java/rocket/planet/controller/admin/AdminDeptController.java
+++ src/main/java/rocket/planet/controller/admin/AdminDeptController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,18 +21,18 @@ public class AdminDeptController {
 	private final AdminDeptService adminDeptService;
 
 	@PostMapping
-	public ResponseEntity<AdminResDto> deptAdd(AdminDeptAddReqDto dto) throws Exception {
+	public ResponseEntity<AdminResDto> deptAdd(@RequestBody AdminDeptAddReqDto dto) throws Exception {
 		return ResponseEntity.ok().body(adminDeptService.addDept(dto));
 	}
 
 	@DeleteMapping
-	public ResponseEntity<AdminResDto> deptRemove(AdminDeptTeamDelReqDto dto) throws Exception {
+	public ResponseEntity<AdminResDto> deptRemove(@RequestBody AdminDeptTeamDelReqDto dto) throws Exception {
 		return ResponseEntity.ok().body(adminDeptService.removeDept(dto));
 	}
 
 	@PutMapping("/name")
 	public ResponseEntity<AdminResDto> deptModify(
-		AdminDeptModReqDto dto) throws Exception {
+		@RequestBody AdminDeptModReqDto dto) throws Exception {
 		return ResponseEntity.ok().body(adminDeptService.modifyDept(dto));
 	}
 }
--- src/main/java/rocket/planet/controller/admin/AdminTeamController.java
+++ src/main/java/rocket/planet/controller/admin/AdminTeamController.java
@@ -1,38 +1,40 @@
-// package rocket.planet.controller.admin;
-//
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-//
-// import lombok.RequiredArgsConstructor;
-// import rocket.planet.dto.admin.AdminDeptTeamDto;
-// import rocket.planet.dto.admin.AdminDeptTeamDto.AdminReqDto;
-// import rocket.planet.dto.admin.AdminDeptTeamDto.AdminResDto;
-// import rocket.planet.service.admin.AdminTeamService;
-//
-// @RestController
-// @RequestMapping("/api/admin/team")
-// @RequiredArgsConstructor
-// public class AdminTeamController {
-//
-// 	private final AdminTeamService adminTeamService;
-//
-// 	@DeleteMapping
-// 	public ResponseEntity<AdminResDto> teamDelete(AdminDeptTeamDto dto) throws Exception {
-// 		return ResponseEntity.ok().body(adminTeamService.removeTeam(dto));
-// 	}
-//
-// 	@PutMapping("/name")
-// 	public ResponseEntity<AdminResDto> teamList(AdminDeptTeamDto dto) throws Exception {
-// 		return ResponseEntity.ok().body(adminTeamService.modifyTeam(dto));
-// 	}
-//
-// 	@PostMapping
-// 	public ResponseEntity<AdminResDto> teamCreate(AdminDeptTeamDto dto) throws Exception {
-// 		return ResponseEntity.ok().body(adminTeamService.addTeam(dto));
-// 	}
-//
-// }
+package rocket.planet.controller.admin;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import rocket.planet.dto.admin.AdminDeptTeamDto.AdminDeptTeamDelReqDto;
+import rocket.planet.dto.admin.AdminDeptTeamDto.AdminResDto;
+import rocket.planet.dto.admin.AdminDeptTeamDto.AdminTeamAddReqDto;
+import rocket.planet.dto.admin.AdminDeptTeamDto.AdminTeamModReqDto;
+import rocket.planet.service.admin.AdminTeamService;
+
+@RestController
+@RequestMapping("/api/admin/team")
+@RequiredArgsConstructor
+public class AdminTeamController {
+
+	private final AdminTeamService adminTeamService;
+
+	@DeleteMapping
+	public ResponseEntity<AdminResDto> teamDelete(@RequestBody AdminDeptTeamDelReqDto dto) throws Exception {
+		return ResponseEntity.ok().body(adminTeamService.removeTeam(dto));
+	}
+
+	@PutMapping("/name")
+	public ResponseEntity<AdminResDto> teamList(AdminTeamModReqDto dto) throws Exception {
+		return ResponseEntity.ok().body(adminTeamService.modifyTeam(dto));
+	}
+
+	@PostMapping
+	public ResponseEntity<AdminResDto> teamAdd(AdminTeamAddReqDto dto) throws Exception {
+		return ResponseEntity.ok().body(adminTeamService.addTeam(dto));
+	}
+
+}
